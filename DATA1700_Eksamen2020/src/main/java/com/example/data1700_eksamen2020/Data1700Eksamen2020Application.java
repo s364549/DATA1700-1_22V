96b7cf568c0d25d25c44b7629caa07102d3e0800
s364549 <s364549@oslomet.no>
2022-04-26 20:24:59 +0200

    -

---
+++ DATA1700_Eksamen2020/src/main/java/com/example/data1700_eksamen2020/Data1700Eksamen2020Application.java
@@ -0,0 +1,13 @@
+package com.example.data1700_eksamen2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data1700Eksamen2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Data1700Eksamen2020Application.class, args);
+	}
+
+}
---
+++ DATA1700_Eksamen2020/src/test/java/com/example/data1700_eksamen2020/Data1700Eksamen2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.data1700_eksamen2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data1700Eksamen2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
